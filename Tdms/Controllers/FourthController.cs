97f6053c8a092b40a438aa8f196a1f12ce10da2e
qqchen <785019257@qq.com>
2022-05-10 13:48:49 +0800

    iactionfilter 扩展

---
+++ Tdms/Controllers/FourthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tdms.Utils.Filters;
+
+namespace Tdms.Controllers
+{
+    public class FourthController : Controller
+    {
+        // GET: Fourth
+        //[MyActionFilterAttribute]
+        [MyActionFilterAttributeBark]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tdms/Utils/Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tdms.Utils.Filters
+{
+    public class MyActionFilterAttribute:ActionFilterAttribute
+    {
+        Logger logger = new Logger(typeof(MyActionFilterAttribute));
+        //方法执行前
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            logger.Info("===OnActionExecuting==");
+            //base.OnActionExecuting(filterContext);
+        }
+        // 方法执行后
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            logger.Info("===OnActionExecuted==");
+            //base.OnActionExecuted(filterContext);
+        }
+
+        //结果执行前
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            logger.Info("===OnResultExecuting==");
+            //base.OnResultExecuting(filterContext);
+        }
+        //结果执行后
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            logger.Info("===OnResultExecuted==");
+            //base.OnResultExecuted(filterContext);
+        }
+    }
+
+    //通过接口来完成：只有方法前，方法后
+    public class MyActionFilterAttributeBark : FilterAttribute, IActionFilter
+    {
+        Logger logger = new Logger(typeof(MyActionFilterAttributeBark));
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            logger.Info("---OnActionExecuting---");
+            //throw new NotImplementedException();
+        }
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            logger.Info("---OnActionExecuted---");
+            //throw new NotImplementedException();
+        }
+
+        
+    }
+}
